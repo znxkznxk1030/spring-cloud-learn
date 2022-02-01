5e46cbfac02b65ab2f88fb7413d42645f8813cba
ysKim <znxkznxk1030@gmail.com>
2022-02-01 21:38:36 +0530

    first commit

---
+++ employee-customer/employee-customer/src/test/java/com/arthur/employeecustomer/EmployeeCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.employeecustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ employee-producer/employee-producer/src/test/java/com/arthur/employeeproducer/EmployeeProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.employeeproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
